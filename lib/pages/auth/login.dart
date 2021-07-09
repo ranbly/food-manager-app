08b8f41359d0b0d9f36d84656c434a14ffb5a3f1
ran.iee <ran.iee@kakaomobility.com>
2021-07-10 03:46:47 +0900

    로그인 화면 레이아웃 작업중

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -8,21 +8,93 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Container(
+        alignment: Alignment.center,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
-              onPressed: () async {
-                try {
-                  EasyLoading.show();
-                  await AuthService.to.signInWithGoogle();
-                } finally {
-                  EasyLoading.dismiss();
-                }
-              },
-              child: Text('구글 로그인'),
-            ),
+            Expanded(
+                flex: 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      '나만의 냉장고 관리',
+                      style: TextStyle(color: Color(0xFF4A5562)),
+                    ),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    Text('마이냉장고',
+                        style: TextStyle(
+                            color: Color(0xFF1C1C1F),
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold)),
+                    Container(
+                      margin: EdgeInsets.only(top: 24),
+                      child: FlutterLogo(
+                        size: 142,
+                      ),
+                    ),
+                  ],
+                )),
+            Expanded(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          minimumSize: Size(double.infinity, 48),
+                        ),
+                        onPressed: () async {
+                          try {
+                            EasyLoading.show();
+                            await AuthService.to.signInWithGoogle();
+                          } finally {
+                            EasyLoading.dismiss();
+                          }
+                        },
+                        child: Container(
+                          child: Text('구글로 로그인',
+                              style: TextStyle(
+                                  color: Color(0xFF686876),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.only(top: 8),
+                        child: OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            minimumSize: Size(double.infinity, 48),
+                            backgroundColor: Color(0xFF141414),
+                          ),
+                          onPressed: () async {
+                            try {
+                              EasyLoading.show();
+                              await AuthService.to.signInWithGoogle();
+                            } finally {
+                              EasyLoading.dismiss();
+                            }
+                          },
+                          child: Container(
+                            child: Text('Apple로 로그인',
+                                style: TextStyle(
+                                    color: Color(0xFFFFFFFF),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500)),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
