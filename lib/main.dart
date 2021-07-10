3789000871821ff2c482d21bcdee5c039d91fc37
GwonHyeok <me@ghyeok.io>
2021-07-11 04:45:29 +0900

    테스트 광고 작업

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_manager_app/pages/app/app.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
 
   await Firebase.initializeApp();
   runApp(App());
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,9 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:food_manager_app/controllers/home.dart';
 import 'package:food_manager_app/screens/refrigerator/screen.dart';
 import 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
+
+  final _bottomBanner = BannerAd(
+      adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.banner,
+      request: AdRequest(),
+      listener: BannerAdListener())
+    ..load();
 
   @override
   Widget build(BuildContext context) {
@@ -25,9 +33,7 @@ class HomePage extends StatelessWidget {
                     Container(
                       color: Colors.white,
                       height: 50,
-                      child: Container(
-                        color: Colors.red,
-                      ),
+                      child: AdWidget(ad: _bottomBanner),
                     )
                   ],
                 ),
